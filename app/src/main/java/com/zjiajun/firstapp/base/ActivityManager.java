29269e1ac5ab5307516b4f46e471317e3c78e617
zhujiajun <zhujiajun.me@gmail.com>
2015-08-02 18:49:54 +0800

    modify

--- app/src/main/java/com/zjiajun/firstapp/base/ActivityManager.java
+++ app/src/main/java/com/zjiajun/firstapp/base/ActivityManager.java
@@ -14,7 +14,8 @@ public class ActivityManager {
     public static List<Activity> activityList = new ArrayList<>();
 
     public static void addActivity(Activity activity) {
-        activityList.add(activity);
+        if (!activityList.contains(activity))
+            activityList.add(activity);
     }
 
     public static void removeActivity(Activity activity) {
